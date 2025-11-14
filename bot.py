3dd7f85fd5b322e9bb2107d41d9c66dc89cb88f1
Aɴɪʀᴜᴅʜ <shadowheroku@gmail.com>
2025-11-14 20:39:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,19 +271,15 @@ class AdvancedPyrogramCleaner(PyrogramChannelCleaner):
                 continue
         return success_count
 
-def main():
-    """Main function to run the bot"""
+async def main():
     API_ID = 21370037
     API_HASH = "0b57036f40bb6da488d05b43e2d20dc1"
     BOT_TOKEN = "8309666031:AAGuAHKvoLY5Q43VOUPvqIbHxEBsUlc0_Ls"
 
     bot = PyrogramChannelCleaner(API_ID, API_HASH, BOT_TOKEN)
-    bot.run()
+    await bot.run()
 
 
 if __name__ == "__main__":
-    main()
-
-
-if __name__ == '__main__':
     asyncio.run(main())
+
